324a2fb40ea2f4703b3f6b71d663111e84bf7787
Gaelu658 <146055738+Gaelu658@users.noreply.github.com>
2023-10-11 11:46:05 +0700

    Add files via upload

---
+++ W 5.2/W 5.2/main.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i=1;
+    while(i<=10,i+=2){
+        printf("%3d",i);
+        ;
+
+    }
+
+    /*int a=0;
+    while(a<5){
+        printf("%3s","Ha");
+
+        a++;
+
+    }
+    /*char x;
+    int i=5;
+    do{
+    puts("Today is a happy day");
+    printf("Do you want to continue?(n= stop)");
+
+    scanf("%c",&x);
+    while(getchar()!='\n');
+
+    }while(x!='n'&& x!='N');
+
+    printf("\n");
+
+        /*printf("%d",i);
+        i++;
+        }while(i<5);
+        printf("\n");
+
+        for(i=5;i<5;i++)
+            printf("%d",i);
+
+    /*int sum;
+    int i;
+    for(i=2;i<=10;i<=10,i+=2){
+        sum+=i;
+        printf("sum=%d i=%d\n",sum,i);
+
+    }
+
+    printf("Sum is %d",sum);
+
+    /* counter,num;
+    int i;
+    for(i=7;i<=77;i+=7){
+    printf("%3d",i);
+
+    }
+
+    /*printf("Input your number:");
+    scanf("%d",&num);
+
+    for( counter =1; counter<=num;counter++){
+
+    if(counter%2) printf("%d\n",counter);
+
+    }
+    puts("Second loop");
+    counter=1;
+    while( counter<=num){
+
+    if(counter%5==0) printf("%d\n",counter);
+    counter++;*/
+
+
+
+    return 0;
+
+}
